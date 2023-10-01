e3034c554413f95feca4b7098a4dc5446993fa2c
Judgment52 <3104405143@qq.com>
2023-10-01 13:53:17 +0900

    Initial commit

---
+++ src/main/java/name/al1s_hikari/HikariClient.java
@@ -0,0 +1,10 @@
+package name.al1s_hikari;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HikariClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
